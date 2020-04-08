a2ea739d440a968d957519a8b86675a2f57c60f4
Dmitriy Visotskiy <visotskiy@surfstudio.ru>
2020-04-08 19:32:23 +0300

    added second activity

---
+++ app/src/main/java/com/example/baseuisample/activity/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.baseuisample.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.baseuisample.R
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var nextScreenBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViews()
+        initListeners()
+    }
+
+    private fun initViews() {
+        nextScreenBtn = findViewById(R.id.open_second_screen_btn)
+    }
+
+    private fun initListeners() {
+        nextScreenBtn.setOnClickListener { openSecondActivity() }
+    }
+
+    private fun openSecondActivity() {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/baseuisample/activity/SecondActivity.kt
@@ -0,0 +1,26 @@
+package com.example.baseuisample.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.baseuisample.R
+
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
